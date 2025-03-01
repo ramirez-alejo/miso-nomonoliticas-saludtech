d5d9ebcb100183af74f19c6fca05b34cef8bc375
Juan Orduz <juanorpinilla@gmail.com>
2025-03-01 17:15:15 -0500

    especificaciones

--- SaludTechDeLosAlpes/bff/main.py
+++ SaludTechDeLosAlpes/bff/main.py
@@ -7,6 +7,8 @@ app.include_router(router, prefix="/graphql")
 # cd saludtechdelosalpes
 # no olvidar instalar los requirements
 # python -m uvicorn bff.main:app --reload
+# http://127.0.0.1:8000/graphql
+#copiar y pegar el archivo en ejemplo.txt y mandar la solicitud
 
 if __name__ == "__main__":
     import uvicorn
