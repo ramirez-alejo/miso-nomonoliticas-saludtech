2d4f6d10089ba35b5e696a7f9c0475a8e5c602e0
Augusto Romero Arango <a.romeroa23@uniandes.edu.co>
2025-02-28 19:15:49 -0500

    Agregar servicio de anonimizacion (#21)

---
+++ SaludTechDeLosAlpes/Ingestion/Ingestion.Aplicacion.Anonimizacion/Modelos/Anonimizar.cs
@@ -0,0 +1,29 @@
+using Consulta.Aplicacion.Dtos;
+
+namespace Ingestion.Aplicacion.Anonimizacion.Modelos;
+
+public class Anonimizar
+{
+	public Guid ImagenId { get; set; }
+	public TipoImagen TipoImagen { get; set; }
+	public AtributosImagen AtributosImagen { get; set; }
+	public string Resolucion { get; set; }
+	public string Contraste { get; set; }
+	public bool Es3D { get; set; }
+	public string FaseEscaner { get; set; }
+
+	public string UbicacionImagen { get; set; }
+	public string ImagenProcesadaPath { get; set; }
+	public string DetalleAnonimizacion { get; set; }
+	
+	
+	
+	
+	public void AnonimizarImagen()
+	{
+		// Anonimizar la imagen
+		ImagenProcesadaPath = $"Imagenes/Anonimizadas/{Guid.NewGuid()}.png";
+		DetalleAnonimizacion = $"Se realizo la anonimizacion de la imagen {ImagenId} agregando filtro borroso a la parte inferior ...";
+		
+	}
+}
