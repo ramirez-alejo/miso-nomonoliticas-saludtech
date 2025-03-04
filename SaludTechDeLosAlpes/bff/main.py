339ddf4a502e6b4e7690c17f557198932d48801c
Juan Orduz <juanorpinilla@gmail.com>
2025-03-04 13:01:37 -0500

    documentacion

--- SaludTechDeLosAlpes/bff/main.py
+++ SaludTechDeLosAlpes/bff/main.py
@@ -8,7 +8,7 @@ app.include_router(router, prefix="/graphql")
 # 3. Ejecuta el servidor con:
 #    uvicorn bff.main:app --host 0.0.0.0 --port 8003 --reload
 # 4. Accede a: http://127.0.0.1:8003/graphql
-
+# 5. copiar el ejemplo.txt y pegarlo en el graphql
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("bff.main:app", host="0.0.0.0", port=8003, reload=True)
